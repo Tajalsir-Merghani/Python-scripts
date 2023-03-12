3c9f9ed57f93a6bc556806ebc9ffbdabb321b8f7
Tajalsir-Merghani <tagalsir45@hotmail.com>
2023-03-12 20:54:58 +0400

    New Verrsion

--- threading.py
+++ threading.py
@@ -3,6 +3,6 @@ import smtplib
 smtObj = smtplib.SMTP('smtp.gmail.com', 587)
 smtObj.ehlo()
 smtObj.starttls()
-smtObj.login('MightyGuy@gmail.com', 'PASSWORD')
+smtObj.login('MightyGuy@gmail.com', 'PASSWORDS')
 smtObj.sendmail("MightyGuy@gmail.com","MightyGuy@hotmail.com",'Subject: SMTP check. \n This is a test email')
 smtObj.quit()
